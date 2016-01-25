c51217ea483d3718169e914cad9c023efabe936b
laras126 <laras126@gmail.com>
2016-01-25 17:09:07 -0500

    add copy to contact

--- location.php
+++ location.php
@@ -4,7 +4,11 @@ include('partials/header.php'); ?>
 
 	<h1>Location</h1>
 	
-	<p>A blurb about contacting us can go here.</p>
+	<p>Getting to the farm isn't that hard.</p>
+
+	<p>Jimmy's Jams is located in Scottdale, PA, about an hour southeast of Pittsburgh. Take the New Stanton exit off of the turnpike and drive 3 miles on the main road. Then make lots of turns - you should probably Google it.</p>
+
+	<!-- Embedded map here -->
 
 <?php include('partials/footer.php'); ?> 
 
