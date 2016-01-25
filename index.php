8932df3b6e6ea469b82a87f45701df6a0f73f420
laras126 <laras126@gmail.com>
2016-01-25 17:09:21 -0500

    change slider image paths

--- index.php
+++ index.php
@@ -1,21 +1,16 @@
 <?php include('partials/header.php') ?>
-
-	<h1>Jimmy's Jams</h1>
-
-	<p id="hello">Hi there!</p>
 	
-	<div class="gallery js-flickity">
+	<div class="gallery">
 		<div class="gallery-cell">
-			<img src="http://placehold.it/800x400">
-			<p>Caption</p>
+			<img src="images/corn-home.jpg" alt="Slider Image">
 		</div>
 		<div class="gallery-cell">
-			<img src="http://placehold.it/800x400">
-			<p>Caption 2</p>
+			<img src="images/elder-home.jpg" alt="Slider Image">
 		</div>
-	</div>  
-
-	<iframe src="https://www.youtube.com/embed/D4cV8yfgNyI" frameborder="0" allowfullscreen></iframe>
+		<div class="gallery-cell">
+			<img src="images/jams-home.jpg" alt="Slider Image">
+		</div>
+  	</div>
 
 	<p>Delicious jams from a Pennsylvania farm.</p>
 	<p>Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Donec id elit non mi porta gravida at eget metus. Donec ullamcorper nulla non metus auctor fringilla. Maecenas faucibus mollis interdum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
