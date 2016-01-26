0c710d758ac501c46e49476e0707751f2703f7c1
laras126 <laras126@gmail.com>
2016-01-25 19:51:34 -0500

    add menu trigger script

--- js/script.js
+++ js/script.js
@@ -3,6 +3,18 @@ $(document).ready(function(){
 
 	$('body').fitVids();
 
+
+	/*
+
+	when user clicks on .menu-trigger
+		addclass nav open
+
+	*/
+
+	$('.menu-trigger').on('click', function() {
+		$('.site-nav').toggleClass('nav-open');
+	});
+
 	// Initiate Flickity plugin
 	$('.gallery').flickity({
   		cellAlign: 'left',
